40246ea575ea1fc898999e606b90fb00e7d67383
RainCity471 <raincity471@gmail.com>
2013-12-18 21:37:03 +0000

    store wait time in variable, stop user from entering nothing for the path first time, and allow file path prompt to show/hide cancel message

--- basic.py
+++ basic.py
@@ -1,23 +1,28 @@
-# basic version--runs from command line
-# needs Python 2.x
-# TO-DO: add error checking facility
-
 import os   # needed for opening/compiling file
 import time # needed for delay
-def getPath():
-    """Ask the user for the file path and return it as string."""
-    path = raw_input("Enter path of lilypond file (including file but without extension), or enter nothing to cancel: ")
+def getPath(allowCancel = True):
+    """Ask the user for lilypond file path and return it as string.
+
+Takes one boolean argument as to whether message should say cancelling is allowed or not. Defaults to true, however this may not be suitable for where the path is needed for initialisation."""
+    if allowCancel == True:
+        question = "Enter path of lilypond file (including file but without extension), or enter nothing to cancel: "
+    else:
+        question = "Enter path of lilypond file (including file but without extension): "
+    path = raw_input(question)
     return path
 
+logwait = 5 # how long the program waits before opening the log
 answer = ""
-path = getPath()
+path = ""
+while path == "":
+    path = getPath(False)
 
 while answer.lower() != "e":
     answer = raw_input("Enter Y or C to compile, E to exit, or P to change file path: ")
     if answer.lower() == "y" or answer.lower() == "c":
         os.startfile(path + ".ly")
-        print "Opening log file in 10 seconds..."
-        time.sleep(10)
+        print "Opening log file in " + str(logwait) + " seconds..."
+        time.sleep(logwait)
         print "Log file: =========================="
         logfile = open(path + ".log", "r")
         print logfile.read()
@@ -25,4 +30,3 @@ while answer.lower() != "e":
         print "===================================="
     elif answer.lower() == "p":
         path = getPath()
-    
