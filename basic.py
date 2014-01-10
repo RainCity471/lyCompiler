6aa3ed3634a22b89f7128883d20f28f65ed00152
RainCity471 <raincity471@gmail.com>
2014-01-10 19:52:46 +0000

    Add semicolons to end of program statements

--- basic.py
+++ basic.py
@@ -1,32 +1,32 @@
-import os   # needed for opening/compiling file
-import time # needed for delay
+import os;   # needed for opening/compiling file
+import time; # needed for delay
 def getPath(allowCancel = True):
     """Ask the user for lilypond file path and return it as string.
 
 Takes one boolean argument as to whether message should say cancelling is allowed or not. Defaults to true, however this may not be suitable for where the path is needed for initialisation."""
     if allowCancel == True:
-        question = "Enter path of lilypond file (including file but without extension), or enter nothing to cancel: "
+        question = "Enter path of lilypond file (including file but without extension), or enter nothing to cancel: ";
     else:
-        question = "Enter path of lilypond file (including file but without extension): "
-    path = raw_input(question)
-    return path
+        question = "Enter path of lilypond file (including file but without extension): ";
+    path = raw_input(question);
+    return path;
 
-logwait = 5 # how long the program waits before opening the log
-answer = ""
-path = ""
+logwait = 5; # how long the program waits before opening the log
+answer = "";
+path = "";
 while path == "":
-    path = getPath(False)
+    path = getPath(False);
 
 while answer.lower() != "e":
-    answer = raw_input("Enter Y or C to compile, E to exit, or P to change file path: ")
+    answer = raw_input("Enter Y or C to compile, E to exit, or P to change file path: ");
     if answer.lower() == "y" or answer.lower() == "c":
-        os.startfile(path + ".ly")
-        print "Opening log file in " + str(logwait) + " seconds..."
-        time.sleep(logwait)
-        print "Log file: =========================="
-        logfile = open(path + ".log", "r")
-        print logfile.read()
-        print "End of log file: ==================="
-        print "===================================="
+        os.startfile(path + ".ly");
+        print "Opening log file in " + str(logwait) + " seconds...";
+        time.sleep(logwait);
+        print "Log file: ==========================";
+        logfile = open(path + ".log", "r");
+        print logfile.read();
+        print "End of log file: ===================";
+        print "====================================";
     elif answer.lower() == "p":
-        path = getPath()
+        path = getPath();
